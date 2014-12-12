ecab3627484d9bdf01211735a31cbb8f5cdef4f7
elou44 <elouarn.laine@gmail.com>
2014-12-12 21:34:10 +0000

    .

--- recup_terrain.py
+++ recup_terrain.py
@@ -2,10 +2,7 @@ import re
 from AmbianceTeam import *
 
 
-init_str = "INIT20ac18ab-6d18-450e-94af-bee53fdc8fcaTO6[2];1;3CELLS:1(23,9)'2'30'8'I,2(41,55)'1'30'8'II,3(23,103)'1'20'5'I;2LINES:1@3433OF2,1@6502OF3"
-
-
-def init_terrain(init_str,listCellules,listInfoTerrain):
+def init_terrain(init_str,listCellules,listLignes,listInfoTerrain):
     
     
     regex1 = re.compile('INIT[A-Za-z0-9-]+TO') # Recuperation de l'id du match
@@ -70,7 +67,7 @@ def init_terrain(init_str,listCellules,listInfoTerrain):
         prod = regex13.findall(cellules_init[i])
         prod = len(prod[0][1:])
         
-        listCellules.append(Cellule(id_cellule,offsize,defsize,radius,x,y,prod))
+        listCellules.append(Cellule(id_cellule,offsize,defsize,radius,x,y,prod)) #instanciation des Cellules
 
     
     
@@ -92,18 +89,9 @@ def init_terrain(init_str,listCellules,listInfoTerrain):
         idcell2 = regex17.findall(lignes_init[j])
         idcell2 = int(idcell2[0][2:])
         
-        listLignes.append(Lines(listCellules[idcell1-1],listCellules[idcell2-1],dist))
+        listLignes.append(Lines(listCellules[idcell1-1],listCellules[idcell2-1],dist)) #instanciation des Lines
         
         
     return listInfoTerrain
         
      
-listInfoTerrain=[]
-listCellules=[]
-listLignes=[]
-
-listInfoTerrain = init_terrain(init_str,listCellules,listInfoTerrain)
-#print(listCellules[0].defsize)
-
-#print(listInfoTerrain[4])
-print(listLignes[1].Cell1.x)
