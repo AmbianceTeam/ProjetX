924b65de8d87dbdc302562fe7fae477cf5876510
elou44 <elouarn.laine@gmail.com>
2015-01-03 16:00:55 +0000

    init_string

--- AmbianceTeam.py
+++ AmbianceTeam.py
@@ -316,10 +316,10 @@ def play_pooo():
     
     # (1) récupère l'état initial 
     # init_state = state()
-    init_state = state()
+    init_string = "INIT20ac18ab-6d18-450e-94af-bee53fdc8fcaTO6[2];1;3CELLS:1(23,9)'2'30'8'I,2(41,55)'1'30'8'II,3(23,103)'1'20'5'I;2LINES:1@3433OF2,1@6502OF3" # donné par le serveur , mais par quelle fonction ? 
     
     # (2) TODO: traitement de init_state
-    Map = init_pooo(init_state) # On initialise Map qui est un objet de type Graphe 
+    Map = init_pooo(init_string) # On initialise Map qui est un objet de type Graphe 
 
     # (3) while True :
     # (4)     state = state_on_update()    
