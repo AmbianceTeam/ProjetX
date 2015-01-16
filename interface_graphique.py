f4f9fa6fe1d62fc95b6e8d922924719a1f62b766
elou44 <elouarn.laine@gmail.com>
2015-01-16 22:19:45 +0000

    changement pour fonctionner avec la nouvelle chaine INIT

--- interface_graphique.py
+++ interface_graphique.py
@@ -7,12 +7,12 @@ def dessiner_terrain(w,Graphe):
     listCellules = Graphe.listCellules
     listLignes = Graphe.listLignes
     listInfoTerrain = Graphe.listInfoTerrain
-    listCouleurs=["#D0D0D0","#4628DC","#C82525"]
+    listCouleurs=["#4628DC","#C82525","#D0D0D0"]
     
     offsetX = 50 # valeur de décalage en X
     offsetY = 50 # valeur de décalage en Y
-    scaleX = 2 # agrandissement du plateau en X
-    scaleY = 2 # agrandissement du plateau en Y
+    scaleX = 30 # agrandissement du plateau en X
+    scaleY = 30 # agrandissement du plateau en Y
     
     w.create_rectangle(0 , 0 , 1000 , 500 , fill="#13084E") # Fond de la fenetre
 
@@ -21,16 +21,16 @@ def dessiner_terrain(w,Graphe):
 
     for j in range (listInfoTerrain[5]): # dessin des lignes
         #Couleur de la ligne
-        if(listLignes[j].Cell1.etat == listLignes[j].Cell2.etat): # si les deux cellules formant la ligne sont de même couleur, alors la ligne est de la même couleur.
-            couleurLigne=listCouleurs[listLignes[j].Cell1.etat]
+        if(listLignes[j].Cell1.couleur == listLignes[j].Cell2.couleur): # si les deux cellules formant la ligne sont de même couleur, alors la ligne est de la même couleur.
+            couleurLigne=listCouleurs[listLignes[j].Cell1.couleur]
         else:
-            couleurLigne=listCouleurs[0] #sinon on met la couleur neutre (gris)
+            couleurLigne=listCouleurs[-1] #sinon on met la couleur neutre (gris)
                 
         w.create_line(listLignes[j].Cell1.x*scaleX+offsetX , listLignes[j].Cell1.y*scaleY+offsetY , listLignes[j].Cell2.x*scaleX+offsetX , listLignes[j].Cell2.y*scaleY+offsetY , fill=couleurLigne,width=3)
 
     for i in range (listInfoTerrain[4]): # dessin des cellules
-        
-        w.create_circle(listCellules[i].x*scaleX+offsetX, listCellules[i].y*scaleY+offsetY, (listCellules[i].radius)*15, fill=listCouleurs[listCellules[i].etat], outline="#FCFCFC", width=2)
+        print('here')
+        w.create_circle(listCellules[i].x*scaleX+offsetX, listCellules[i].y*scaleY+offsetY, (listCellules[i].radius)/4, fill=listCouleurs[listCellules[i].couleur], outline="#FCFCFC", width=2)
         w.create_text(listCellules[i].x*scaleX+offsetX,listCellules[i].y*scaleY+offsetY,text=str(listCellules[i].nboff),fill="white",anchor="s")
         w.create_text(listCellules[i].x*scaleX+offsetX,listCellules[i].y*scaleY+offsetY,text="I"*listCellules[i].prod,fill="white",anchor="n")
     
@@ -53,8 +53,8 @@ window = Tk() # Création de l'objet window (fenetre)
 w = Canvas(window , width=1000, height =500) # Création d'un Canvas à partir de la fenetre "window"
 w. pack ()
 
-init_str = "INIT20ac18ab-6d18-450e-94af-bee53fdc8fcaTO6[2];1;3CELLS:1(23,9)'2'30'8'I,2(41,55)'1'30'8'II,3(23,103)'1'20'5'I;2LINES:1@3433OF2,1@6502OF3"
-
+init_str = "INIT013fe2e8-86df-4fa3-960d-a0a537849a68TO2[1];2;7CELLS:0(0,0)'100'30'8'I,1(0,5)'100'30'8'I,2(5,0)'100'30'8'I,3(5,5)'200'30'8'II,4(5,10)'100'30'8'I,5(10,5)'100'30'8'I,6(10,10)'100'30'8'I;6LINES:0@4800OF1,0@4800OF2,2@4700OF3,3@4700OF4,4@4800OF6,5@4800OF6"
+#init_str = "INIT20ac18ab-6d18-450e-94af-bee53fdc8fcaTO6[2];1;3CELLS:1(23,9)'2'30'8'I,2(41,55)'1'30'8'II,3(23,103)'1'20'5'I;2LINES:1@3433OF2,1@6502OF3"
 Map = init_pooo(init_str)
 
 print(Map.listLignes[1].Cell1.x)
