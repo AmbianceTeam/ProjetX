5277562fa869bafd04c657b8dcdc4e5db3ffb1e4
elou44 <elouarn.laine@gmail.com>
2015-01-16 17:15:57 +0000

    debug

--- AmbianceTeam.py
+++ AmbianceTeam.py
@@ -388,7 +388,7 @@ def setmove(userid,pourcent,Cellfrom,Cellto):
     return res
 
 
-def main() :
+"""def main() :
     
     init_string = "INIT20ac18ab-6d18-450e-94af-bee53fdc8fcaTO6[2];1;3CELLS:1(23,9)'2'30'8'I,2(41,55)'1'30'8'II,3(23,103)'1'20'5'I;2LINES:1@3433OF2,1@6502OF3"
     init_pooo(init_string) # Instanciation de la Map (objet Graphe)
@@ -405,4 +405,4 @@ def main() :
 
     
 if __name__ == '__main__':
-    main()
+    main()"""
